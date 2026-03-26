c87752aba5497126b84b1ddb8ce31458b5ba7def
Elias Camargo <159282125+EliasKmrgo@users.noreply.github.com>
2026-03-26 11:37:13 -0500

    Add JPA repositories and entity scanning configuration

--- src/main/java/co/edu/uptc/personservice/PersonServiceApplication.java
+++ src/main/java/co/edu/uptc/personservice/PersonServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "co.edu.uptc.personservice.repository")
+@EntityScan(basePackages = "co.edu.uptc.personservice.model")
 public class PersonServiceApplication {
 
     public static void main(String[] args) {

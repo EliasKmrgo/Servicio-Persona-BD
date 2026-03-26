0145e2f2b2a82246b4d58d5a6117d22b25e22c90
Elias Camargo <159282125+EliasKmrgo@users.noreply.github.com>
2026-03-26 11:36:21 -0500

    Add @Repository annotation to PersonRepository

--- src/main/java/co/edu/uptc/personservice/repository/PersonRepository.java
+++ src/main/java/co/edu/uptc/personservice/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package co.edu.uptc.personservice.repository;
 
 import co.edu.uptc.personservice.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository; // <--- Agregar esta
 
+@Repository // <--- Agregar esta anotación
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
